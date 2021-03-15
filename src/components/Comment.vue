cfe75956b6254eb9695dfd3ee930c61076bd9c70
Dallen Pyrah <dallenpyrah@gmail.com>
2021-03-15 11:41:51 -0600

    fixed

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="col-10">
-    <router-link :to="{name:'BlogDetails', params: {id: comments.blog}}">
+    <router-link :to="{name:'BlogDetails', params: {id: comments.blog}}" v-if="isAccountPage">
       <div class="card comment text-white bg-light">
         <div class="card-body">
           <h4 class="card-title text-dark">
@@ -19,6 +19,23 @@
         </div>
       </div>
     </router-link>
+    <div v-else class="card text-white bg-light">
+      <div class="card-body">
+        <h4 class="card-title text-dark">
+          <div v-if="comments.creator && comments.creator.email == state.user.email">
+            <i class="fa fa-trash text-danger" aria-hidden="true" @click="deleteComment"></i>
+            {{ comments.body }}
+          </div>
+          <div v-else>
+            {{ comments.body }}
+          </div>
+        </h4>
+        <p class="card-title text-dark">
+          <img :src="comments.creator.picture" class="rounded mr-2" width="50" alt="">
+          <span v-if="comments">{{ comments.creator.name }}</span>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -27,6 +44,7 @@ import { computed, reactive } from 'vue'
 import { AppState } from '../AppState'
 import { commentsService } from '../services/CommentsService'
 import { blogsService } from '../services/BlogsService'
+import { useRoute } from 'vue-router'
 
 export default {
   name: 'Comment',
@@ -34,12 +52,14 @@ export default {
     comments: { type: Object, required: true }
   },
   setup(props) {
+    const route = useRoute()
     const state = reactive({
       blogPage: true,
       user: computed(() => AppState.user),
       comments: computed(() => AppState.comments)
     })
     return {
+      isAccountPage: computed(() => route.name === 'Account'),
       state,
       async deleteComment() {
         try {
